33767c0e874a153be7ed937800aef97a84ec9cb5
mvorotniak <v-mvorotniak@vrbo.com>
2021-12-18 02:02:12 -0800

    initial commit

---
+++ src/main/java/com/mvoro/developer/springmvcrecipeproject/SpringMvcReceipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mvoro.developer.springmvcrecipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcReceipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcReceipeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mvoro/developer/springmvcrecipeproject/SpringMvcReceipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvoro.developer.springmvcrecipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcReceipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
