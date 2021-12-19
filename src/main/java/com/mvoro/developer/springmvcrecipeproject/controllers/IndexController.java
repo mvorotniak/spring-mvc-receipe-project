a82104bfb0769d583608f9f311703fdb35d7d4a2
mvorotniak <v-mvorotniak@vrbo.com>
2021-12-19 01:57:30 -0800

    Create index page

---
+++ src/main/java/com/mvoro/developer/springmvcrecipeproject/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.mvoro.developer.springmvcrecipeproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String index() {
+        return "index";
+    }
+}
