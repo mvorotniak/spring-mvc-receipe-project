89b7a12140130175bdd61865939f4d946dfd426c
mvorotniak <v-mvorotniak@vrbo.com>
2021-12-29 07:09:55 -0800

    App name refactoring

--- src/main/java/com/mvoro/developer/springmvcrecipeproject/SpringMvcRecipeProjectApplication.java
+++ src/main/java/com/mvoro/developer/springmvcrecipeproject/SpringMvcRecipeProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringMvcReceipeProjectApplication {
+public class SpringMvcRecipeProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringMvcReceipeProjectApplication.class, args);
+        SpringApplication.run(SpringMvcRecipeProjectApplication.class, args);
     }
 
 }
--- src/test/java/com/mvoro/developer/springmvcrecipeproject/SpringMvcRecipeProjectApplicationTests.java
+++ src/test/java/com/mvoro/developer/springmvcrecipeproject/SpringMvcRecipeProjectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringMvcReceipeProjectApplicationTests {
+class SpringMvcRecipeProjectApplicationTests {
 
     @Test
     void contextLoads() {
