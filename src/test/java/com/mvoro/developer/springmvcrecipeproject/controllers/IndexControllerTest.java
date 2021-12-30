ce1c745b928bed42e57c3cbf2a4708e6f23ae6e3
mvorotniak <v-mvorotniak@vrbo.com>
2021-12-30 10:39:21 -0800

    Using MockMvc for endpoint testing

--- src/test/java/com/mvoro/developer/springmvcrecipeproject/controllers/IndexControllerTest.java
+++ src/test/java/com/mvoro/developer/springmvcrecipeproject/controllers/IndexControllerTest.java
@@ -8,15 +8,20 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import com.mvoro.developer.springmvcrecipeproject.domain.Recipe;
 import com.mvoro.developer.springmvcrecipeproject.services.RecipeService;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @ExtendWith(MockitoExtension.class)
 class IndexControllerTest {
@@ -32,6 +37,18 @@ class IndexControllerTest {
     @BeforeEach
     void setUp() {
         indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void mockMvc() throws Exception {
+        // Easy way to test an endpoint without creating an Integration test
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        // Verify that by calling the `index` endpoint we obtain a 200 OK response code
+        // and a view called `index`
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("index"));
     }
 
     @Test
