9b78bb3ed32918d2ddd62a760e34647fcaf3d16c
Mariana Vorotniak <mvorodeveloper@gmail.com>
2022-05-22 04:00:49 -0700

    Show image on recipe page

--- src/main/java/com/mvoro/developer/springmvcrecipeproject/controllers/ImageController.java
+++ src/main/java/com/mvoro/developer/springmvcrecipeproject/controllers/ImageController.java
@@ -1,5 +1,13 @@
 package com.mvoro.developer.springmvcrecipeproject.controllers;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.http.HttpResponse;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +16,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.mvoro.developer.springmvcrecipeproject.commands.RecipeCommand;
 import com.mvoro.developer.springmvcrecipeproject.services.ImageService;
 import com.mvoro.developer.springmvcrecipeproject.services.RecipeService;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 
 @Slf4j
 @Controller
@@ -38,8 +48,30 @@ public class ImageController {
 
     @PostMapping("recipes/{id}/image")
     public String saveImage(@PathVariable Long id, @RequestParam("imageFile") MultipartFile file) {
-        imageService.uploadImageForRecipeId(id, file);
+        this.imageService.uploadImageForRecipeId(id, file);
 
         return "redirect:/recipes/" + id + "/show";
+    }
+
+    @GetMapping("recipes/{id}/recipeImage")
+    public void loadImage(@PathVariable Long id, final HttpServletResponse response) throws IOException {
+        final RecipeCommand recipeCommand = this.recipeService.findCommandById(id);
+
+        if (recipeCommand.getImage() != null) {
+            log.info("Loading image for recipe [{}]", id);
+            final byte[] image = new byte[recipeCommand.getImage().length];
+
+            int i = 0;
+            for (Byte imageByte : recipeCommand.getImage()) {
+                image[i++] = imageByte; // unboxing
+            }
+
+            response.setContentType("image/jpeg");
+            final InputStream inputStream = new ByteArrayInputStream(image);
+            IOUtils.copy(inputStream, response.getOutputStream());
+            log.info("Successfully loaded image for recipe [{}]", id);
+        } else {
+            log.warn("The provided recipe with id [{}] does not have an image.", id);
+        }
     }
 }
