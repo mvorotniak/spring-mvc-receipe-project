87744a8fc2bfc3d84f861787bf8d236b58145cfc
Mariana Vorotniak <mvorodeveloper@gmail.com>
2022-10-31 01:32:27 -0700

    Added SQL script generation for DB schema

--- src/main/java/com/mvoro/developer/springmvcrecipeproject/SpringMvcRecipeProjectApplication.java
+++ src/main/java/com/mvoro/developer/springmvcrecipeproject/SpringMvcRecipeProjectApplication.java
@@ -2,8 +2,9 @@ package com.mvoro.developer.springmvcrecipeproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {H2ConsoleAutoConfiguration.class})
 public class SpringMvcRecipeProjectApplication {
 
     public static void main(String[] args) {
